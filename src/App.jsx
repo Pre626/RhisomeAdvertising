3d23a32ef9f013bb5c4e3e387393feb1aabb4bee
Prerana6051 <preranaattarde2001@gmail.com>
2025-04-10 11:06:50 -0700

    Home UI

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="hero-container">
+      <h1 className="main-heading">
+  RHISOME <br /> ADVERTISING
+</h1>
+
+<div className="icon-container">
+  <img src="/comments.png" className="icon icon1" alt="comment" />
+  <img src="/love.png" className="icon icon2" alt="like" />
+  <img src="/like.png" className="icon icon3" alt="like" />
+  <img src="/like.png" className="icon icon4" alt="like" />
+  <img src="/astronaut.png" className="astronaut" alt="astronaut" />
+</div>
+</div>
+  );
+}
+
+export default App;
